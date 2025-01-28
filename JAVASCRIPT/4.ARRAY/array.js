d3a5c68460402eeff1c2e895b61b41d10a4da605
rubesh286 <rubesh2864@GMAIL.COM>
2025-01-28 17:11:07 +0530

    most recent

---
+++ JAVASCRIPT/4.ARRAY/array.js
@@ -0,0 +1,62 @@
+
+let arr=[10,'hello',true,30]
+
+console.log(arr);
+
+// ! how to get the length of the array
+
+console.log(arr.length);
+
+// ! array Methods
+
+// ! 1. push()
+
+//  it is used to add element at the end of the array.
+
+
+arr.push("byee")
+console.log(arr);
+
+
+// ! 2.pop()
+
+let names=['rohit','virat','dhoni','ruthuraj gaikwad']
+
+names.pop()
+
+console.log(names);
+
+
+// ! 3. unshift
+
+// it is used to add the element the starting of the array.
+
+names.unshift("dhawan")
+
+console.log(names);
+
+// ! 4. SHIFT()
+
+let fruits=['mango','banana','apple']
+
+fruits.shift()
+console.log(fruits);
+
+
+
+// ! 5.indexof()
+
+let subjects=['html','css','java','sql']
+
+console.log(subjects.indexOf("java"))
+
+
+// ! 6. INCLUDES
+
+// iT IS USED TO CHECK WHEATHER THE ELEMNET ARE PRESENT OR NOT IN THE ARRAY
+
+console.log(subjects.includes('html'));
+
+
+
+
